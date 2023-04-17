b3353fc4986d1fc292bcca5026be38e35b994b56
JoaoProfeta <amvjoao@hotmail.com>
2023-04-16 21:33:34 -0300

    corrigindo bug que adicionava [] atoa no logged in

--- src/Components/ProjectForm/ProjectForm.jsx
+++ src/Components/ProjectForm/ProjectForm.jsx
@@ -13,45 +13,72 @@ export const ProjectForm = ({ handleSubmit, btnText, projectData }) => {
     const [projects, setProjects] = useState({});
     const [userProjects, setUserProjects] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState(0)
-    useEffect(() => {
-        const getCategories = JSON.parse(localStorage.getItem("categories"));
-        setCategories(getCategories);
 
-        const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
-        const projects = loggedInUser.projects || [];
-        setUserProjects(projects);
+
+
+    const userLoggedIn = JSON.parse(localStorage.getItem("loggedInUser"))
+
+    const logged = !!userLoggedIn
+
+
+
+    useEffect(() => {
+
+
+        if (logged) {
+            const getCategories = JSON.parse(localStorage.getItem("categories"));
+            setCategories(getCategories);
+
+            const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+            const projects = loggedInUser.projects || [];
+            setUserProjects(projects);
+        }
+
+
     }, [])
+
+
 
     function handleChange(e) {
         setProjects({ ...projects, [e.target.name]: e.target.value });
     }
 
+
+
     function handleCategory(e) {
 
         setSelectedCategory(e.target.value)
-        
+
     }
+
+
 
     function handleSubmitForm(e) {
         e.preventDefault();
         const categoryFind = categories.find(c => c.id == selectedCategory);
-        
-        const newProject = { name: projects.name, budget: projects.budget, id: projects.id, category:categoryFind.name };
+
+        const newProject = { name: projects.name, budget: projects.budget, id: projects.id, category: categoryFind.name };
         setUserProjects([...userProjects, newProject]);
-
-
     }
+
+
 
     useEffect(() => {
 
-        const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+        if (logged) {
 
-        const updatedUser = { ...loggedInUser, projects: userProjects };
+            const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
 
-        localStorage.setItem('loggedInUser', JSON.stringify(updatedUser));
-        
+            const updatedUser = { ...loggedInUser, projects: userProjects };
+
+            localStorage.setItem('loggedInUser', JSON.stringify(updatedUser));
+        }
+
     }, [userProjects]);
-   
+
+
+
+
     return (
         <FormForProjects onSubmit={handleSubmitForm}>
             <Input
