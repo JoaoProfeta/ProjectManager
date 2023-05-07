a36c4aa070e463807c8cbde09c745910fa4f470d
JoaoProfeta <amvjoao@hotmail.com>
2023-05-07 18:52:20 -0300

    add strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
+import { react }from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
-	<App/>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 
 );
